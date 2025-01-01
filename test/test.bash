28e98e12ab2e48665b627bb8198a88e82c268f62
Akira Matsumoto <akiram8427@gmail.com>
2025-01-02 00:32:41 +0900

    しらみつぶし4

--- test/test.bash
+++ test/test.bash
@@ -10,6 +10,7 @@ colcon build
 source $dir/.bashrc
 
 ros2 run random_service random_generator &
+ps aux | grep random_generator
 NODE_PID=$!
-ros2 service list
+ps aux | grep random_generator
 kill $NODE_PID
