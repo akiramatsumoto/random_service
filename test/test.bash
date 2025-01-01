cf1c5bc8a0c11962974827487a3e4289397f1f79
Akira Matsumoto <akiram8427@gmail.com>
2025-01-01 23:21:27 +0900

    最低限のテスト

--- test/test.bash
+++ test/test.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+cleanup() {
+    kill $NODE_PID
+    exit 1
+}
+
 dir=~
 [ "$1" != "" ] && dir="$1"
 
@@ -9,4 +14,14 @@ source $dir/.bashrc
 
 ros2 run random_service random_generator &
 NODE_PID=$!
+ros2 service list | grep /query
+[ $? = 0 ] || cleanup
+ros2 service call /query random_service/srv/Query "num_digit: 1"
+[ $? = 0 ] || cleanup
+ros2 service call /query random_service/srv/Query "num_digit: -1"
+[ $? = 1 ] || cleanup
+ros2 service call /query random_service/srv/Query "num_digit: -1"
+[ $? = 1 ] || cleanup
 kill $NODE_PID
+echo "テストは成功しました"
+exit 0
